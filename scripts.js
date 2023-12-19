d826435dec83f6d78060d3f3a9d7b0085694397a
Caio Henrique dos Santos Correa Alves <caio.alves@franqueadora.com.br>
2023-12-19 10:30:07 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach(element => myObserver.observe(element))
